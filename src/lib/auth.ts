1ee33ddec229ddae092cbab33a30cc57baa217e3
umarluqman <umarluqman.78@gmail.com>
2025-12-26 15:23:35 +0800

    init wrangler

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
-import { betterAuth } from "better-auth"
-import { drizzleAdapter } from "better-auth/adapters/drizzle"
-import { tanstackStartCookies } from "better-auth/tanstack-start"
-import { db } from "@/db/connection"
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { db } from "@/db/connection";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -14,7 +14,10 @@ export const auth = betterAuth({
 
   baseURL: process.env.VITE_BASE_URL || "http://localhost:3000",
 
-  trustedOrigins: ["http://localhost:3000"],
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://tanstack-start-app.dialisis-admin.workers.dev",
+  ],
 
   plugins: [tanstackStartCookies()],
-})
+});
